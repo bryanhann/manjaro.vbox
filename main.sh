bc9f9fc5635ea6364607b573c1f557a1adf2be7b
Bryan Hann <nobody@nowhere>
2021-08-06 16:19:14 -0230

    Iterate over numerically ordered scripts.

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
-echo $(dirname $0)
+[ "$0" = "./main.sh" ] || echo try './main.sh'
+[ "$0" = "./main.sh" ] || exit
+
+export ROOT=${PWD}
+export SCRIPTS=${ROOT}/scripts
+
+echo ROOT is ${ROOT}
+
+for script in $(ls ${SCRIPTS} | sort | grep ^[0-9]); do
+    echo '--> ' $script
+    ${SCRIPTS}/${script}
+done
+
